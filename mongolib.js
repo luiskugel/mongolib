a467502bbf21b3d49a04ef14c13737316b7032fa
Luis Kugel <luis@kugelx.de>
2020-11-21 13:19:52 +0100

    added class to fix performance issue

--- mongolib.js
+++ mongolib.js
@@ -1,89 +1,208 @@
-const MongoClient = require('mongodb').MongoClient
-module.exports.deleteDocument =  function(config, query){
-  return new Promise((resolve, reject) => {
-    MongoClient.connect(config.url, {useUnifiedTopology: true}, function(err, db) {
-      if (err) console.log(err)
-      var dbo = db.db(config.dbName);
-      dbo.collection(config.dbCollection).deleteOne(query, function(err, obj) {
-        if (err) {
-          console.log(err)
-          reject(err)
-        }else{
-          resolve()
-        }
-        db.close();
-      });
-    });
-    })
+const MongoClient = require("mongodb").MongoClient;
+
+class MongoLib {
+  constructor(dbname, collection) {
+    this.dbname = dbname;
+    this.collection = collection;
   }
-  
-module.exports.insertDocument =  function(config, obj){
-  return new Promise((resolve, reject) => {
-    MongoClient.connect(config.url, {useUnifiedTopology: true}, function(err, db) {
-      if (err) console.log(err)
-      var dbo = db.db(config.dbName);
-      dbo.collection(config.dbCollection).insertOne(obj, function(err, res) {
-        if (err) {
-          console.log(err)
-          reject(err)
-        }else{
-          resolve()
+  connect(url) {
+    MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
+      if (err) console.log(err);
+      this.db = db;
+    });
+  }
+  delete(query, dbname = this.dbname, collection = this.collection) {
+    return new Promise((resolve, reject) => {
+      _getCollectionObj(dbname, collection).deleteOne(
+        query,
+        function (err, obj) {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            resolve();
+          }
         }
-        db.close(); 
+      );
+    });
+  }
+  insert(obj, dbname = this.dbname, collection = this.collection) {
+    return new Promise((resolve, reject) => {
+      _getCollectionObj(dbname, collection).insertOne(obj, function (err, res) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(res);
+        }
       });
     });
-  })
+  }
+  get(
+    query,
+    field_selector = {},
+    dbname = this.dbname,
+    collection = this.collection
+  ) {
+    return new Promise((resolve, reject) => {
+      this._getCollectionObj(dbname, collection)
+        .find(query)
+        .project(field_selector)
+        .toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+            reject(err);
+          }
+          resolve(result);
+        });
+    });
+  }
+  update(
+    query,
+    updatedValues,
+    dbname = this.dbname,
+    collection = this.collection
+  ) {
+    return new Promise((resolve, reject) => {
+      this._getCollectionObj(dbname, collection).update(
+        query,
+        updatedValues,
+        function (err, res) {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            resolve(res);
+          }
+        }
+      );
+    });
+  }
+  _getCollectionObj(dbname, collection) {
+    return this.db.db(dbname).collection(collection);
+  }
+  close() {
+    this.db.close();
+  }
 }
 
-module.exports.getDocument =  function(config, query, field_selector={}){
+module.exports.deleteDocument = function (config, query) {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(config.url, {useUnifiedTopology: true}, function(err, db) {
-      if (err) console.log(err)
-      var dbo = db.db(config.dbName);
-      dbo.collection(config.dbCollection).find(query).project(field_selector).toArray(function(err, result) {
-        if (err) {
-          console.log(err)
-          reject(err)
-        } 
-        db.close();
-        resolve(result)
-      });
-    });
-  })
-}
+    MongoClient.connect(
+      config.url,
+      { useUnifiedTopology: true },
+      function (err, db) {
+        if (err) console.log(err);
+        var dbo = db.db(config.dbName);
+        dbo
+          .collection(config.dbCollection)
+          .deleteOne(query, function (err, obj) {
+            if (err) {
+              console.log(err);
+              reject(err);
+            } else {
+              resolve();
+            }
+            db.close();
+          });
+      }
+    );
+  });
+};
 
-module.exports.updateDocument =  function(config, query, updatedValues){
+module.exports.insertDocument = function (config, obj) {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(config.url, {useUnifiedTopology: true}, function(err, db) {
-      if (err) console.log(err)
-      var dbo = db.db(config.dbName);
-      dbo.collection(config.dbCollection).updateOne(query, updatedValues, function(err, res) {
-        if (err) {
-          console.log(err)
-          reject(err)
-        }else{
-          resolve()
-        }
-        db.close();
-      });
-    });
-  })
-}
+    MongoClient.connect(
+      config.url,
+      { useUnifiedTopology: true },
+      function (err, db) {
+        if (err) console.log(err);
+        var dbo = db.db(config.dbName);
+        dbo.collection(config.dbCollection).insertOne(obj, function (err, res) {
+          if (err) {
+            console.log(err);
+            reject(err);
+          } else {
+            resolve();
+          }
+          db.close();
+        });
+      }
+    );
+  });
+};
 
-module.exports.insertManyDocuments =  function(config, array){
+module.exports.getDocument = function (config, query, field_selector = {}) {
   return new Promise((resolve, reject) => {
-    MongoClient.connect(config.url, {useUnifiedTopology: true}, function(err, db) {
-      if (err) console.log(err)
-      var dbo = db.db(config.dbName);
-      dbo.collection(config.dbCollection).insertMany(array, function(err, res) {
-        if (err) {
-          console.log(err)
-          reject(err)
-        }else{
-          resolve()
-        }
-        db.close(); 
-      });
-    });
-  })
-}
+    MongoClient.connect(
+      config.url,
+      { useUnifiedTopology: true },
+      function (err, db) {
+        if (err) console.log(err);
+        var dbo = db.db(config.dbName);
+        dbo
+          .collection(config.dbCollection)
+          .find(query)
+          .project(field_selector)
+          .toArray(function (err, result) {
+            if (err) {
+              console.log(err);
+              reject(err);
+            }
+            db.close();
+            resolve(result);
+          });
+      }
+    );
+  });
+};
+
+module.exports.updateDocument = function (config, query, updatedValues) {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(
+      config.url,
+      { useUnifiedTopology: true },
+      function (err, db) {
+        if (err) console.log(err);
+        var dbo = db.db(config.dbName);
+        dbo
+          .collection(config.dbCollection)
+          .updateOne(query, updatedValues, function (err, res) {
+            if (err) {
+              console.log(err);
+              reject(err);
+            } else {
+              resolve();
+            }
+            db.close();
+          });
+      }
+    );
+  });
+};
+
+module.exports.insertManyDocuments = function (config, array) {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(
+      config.url,
+      { useUnifiedTopology: true },
+      function (err, db) {
+        if (err) console.log(err);
+        var dbo = db.db(config.dbName);
+        dbo
+          .collection(config.dbCollection)
+          .insertMany(array, function (err, res) {
+            if (err) {
+              console.log(err);
+              reject(err);
+            } else {
+              resolve();
+            }
+            db.close();
+          });
+      }
+    );
+  });
+};
+
+module.exports.MongoLib = MongoLib;
